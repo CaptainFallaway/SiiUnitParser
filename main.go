1308e08a5ac17d8700fe6f8f6376077952259dff
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-10-29 04:43:11 +0100

    Some fun testing stuff

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/CaptainFallaway/SiiUnitParser/pkg/siiunit"
 )
@@ -14,14 +15,39 @@ func main() {
 	}
 	defer file.Close()
 
-	units, err := siiunit.ParseAllUnitsConcurrent(file)
+	start := time.Now()
+
+	units, err := siiunit.ParseAllUnitsConcurrent(file, siiunit.OptWorkerCount(8))
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Parsed", len(units), "units")
+	elapsed := time.Since(start)
 
 	for _, unit := range units {
 		fmt.Println(unit)
 	}
+
+	unit := units[0]
+
+	unlockedDealersAttr, _ := unit.Attrs.Get("unlocked_dealers")
+
+	unlockedDealersAttrs, err := unlockedDealersAttr.Arr()
+	if err != nil {
+		panic(err)
+	}
+
+	var unlockedDealers []string
+	for _, attr := range unlockedDealersAttrs {
+		strVal, err := attr.String()
+		if err != nil {
+			panic(err)
+		}
+		unlockedDealers = append(unlockedDealers, strVal)
+	}
+
+	fmt.Println(unlockedDealers)
+
+	fmt.Println("Parsed", len(units), "units")
+	fmt.Println("Parsing took", elapsed)
 }
